ba630b66a6ebd80f7efbaf3c5eda48e9497d386c
Stefano Consonni <stefanoconsonni@ymail.com>
2022-08-19 17:56:27 +0200

    complete app

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,41 +1,45 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
-export const useFetch = (url) => {
-	const [data, setData] = useState(null);
-	const [isPending, setIsPending] = useState(false);
-	const [error, setError] = useState(null);
+export const useFetch = (url, _options) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
 
-	useEffect(() => {
-		const controller = new AbortController();
+  // use useRef to wrap an object/array argument which is a useEffect dependency
+  const options = useRef(_options).current;
 
-		const fetchData = async () => {
-			setIsPending(true);
+  useEffect(() => {
+    console.log(options);
+    const controller = new AbortController();
 
-			try {
-				const res = await fetch(url, { signal: controller.signal });
-				if (!res.ok) {
-					throw new Error(res.statusText);
-				}
-				const json = await res.json();
+    const fetchData = async () => {
+      setIsPending(true);
 
-				setIsPending(false);
-				setData(json);
-				setError(null);
-			} catch (err) {
-				if (err.name === "AbortError") {
-					console.log("The fetch was aborted");
-				} else {
-					setIsPending(false);
-					setError("Could not fetch the data");
-				}
-			}
-		};
-		fetchData();
+      try {
+        const res = await fetch(url, { signal: controller.signal });
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        const json = await res.json();
 
-		return () => {
-			controller.abort();
-		};
-	}, [url]);
+        setIsPending(false);
+        setData(json);
+        setError(null);
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log("The fetch was aborted");
+        } else {
+          setIsPending(false);
+          setError("Could not fetch the data");
+        }
+      }
+    };
+    fetchData();
 
-	return { data: data, isPending, error };
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  return { data: data, isPending, error };
 };
